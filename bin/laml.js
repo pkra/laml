0b342727e95341692a03e3f261eb9d00696f7a6b
Peter Krautzberger <p.krautzberger@gmail.com>
2018-01-17 21:14:57 +0100

    [bin/laml.js] support HTML fragments * add CLI options -i -o (mandatory) -f (optional) Resolves #4

--- bin/laml.js
+++ bin/laml.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 const jsdom = require('jsdom');
@@ -5,11 +6,18 @@ const virtualConsole = new jsdom.VirtualConsole();
 virtualConsole.sendTo(console);
 const { JSDOM } = jsdom;
 
-const {worker} = require('./worker.js');
+const {worker} = require('../src/worker.js');
 
 // process CLI arguments
-const input = fs.readFileSync(process.argv[2]).toString();
-const outputname = process.argv[3];
+const argv = require('minimist')(process.argv.slice(2));
+
+if (!argv.i || !argv.o) {
+  console.log('Required arguments: -i path/to/input.html -o path/to/output.html | Optional: -f [fragment]')
+}
+
+const input = fs.readFileSync(argv.i).toString();
+const outputname = argv.o;
+const isFragment = Boolean(argv.f);
 
 const dom = new JSDOM(input, {
   virtualConsole: virtualConsole,
@@ -19,8 +27,13 @@ const window = dom.window;
 
 worker(window);
 
+// move anything that ends up in the head (e.g., leading script tags) back to the body
+if (isFragment){
+  for (let node of window.document.querySelectorAll('head *')) window.document.body.insertBefore(node, window.document.body.firstChild);
+}
+
 // write out
-let output = dom.serialize();
+const output = isFragment ? window.document.body.innerHTML : dom.serialize();
 fs.writeFileSync(outputname, output);
 // clean up
 window.close();
