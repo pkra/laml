74cd8adc0be216d391d62ff4f399255852812e5a
Peter Krautzberger <p.krautzberger@gmail.com>
2018-01-24 00:02:25 +0100

    [blame] only create heading when no preceding name Fixes #29

--- src/blames.js
+++ src/blames.js
@@ -4,7 +4,12 @@ module.exports = function(document) {
   // convert blames to headings
   const blames = document.querySelectorAll('blame');
   for (let blame of blames) {
+    const previous = blame.previousElementSibling;
+    if (previous && (previous.classList.contains('name') || previous.tagName === 'NAME')) {
+      const renamedNode = renameTag(document, blame, 'span', true);
+      previous.appendChild(renamedNode);
+    }
     // TODO look up correct heading level
-    const renamedNode = renameTag(document, blame, 'h2', true);
+    else {const renamedNode = renameTag(document, blame, 'h2', true);}
   }
 };
