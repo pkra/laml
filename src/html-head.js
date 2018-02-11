650ac99c20b321bec26d235ba6bc64957ab87a00
Peter Krautzberger <p.krautzberger@gmail.com>
2018-02-11 19:42:52 +0100

    add html-head.js Fixes head when HTML fragment is given Resolves #38

---
+++ src/html-head.js
@@ -0,0 +1,25 @@
+const metaTagGenerator = function(){
+    // expects at least one string of type `attribute="value"`
+    const argArr = [...arguments];
+    const checkForElement = document.querySelector('head [' + argArr.join('][') + ']');
+    if (!checkForElement){
+        const metaElement = document.createElement('meta');
+        for (let argument of argArr){
+            const split = argument.split('="');
+            metaElement.setAttribute(split[0], split[1].substring(0, split[1].length-1));
+        }
+        document.head.appendChild(metaElement);
+        return metaElement;
+    }
+    else return checkForElement;
+}
+module.exports = function(document) {
+    // some good practices (especially for fragments)
+    // add <meta charset="UTF-8">
+    metaTagGenerator('charset="UTF-8"');
+    // <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    metaTagGenerator('name="viewport"', 'content="width=device-width, initial-scale=1.0"')
+    // <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    metaTagGenerator('http-equiv="X-UA-Compatible"', 'content="ie=edge"');
+
+};
