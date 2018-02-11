4af838bd72bdfc5ab994759d389136ef44024459
Peter Krautzberger <p.krautzberger@gmail.com>
2018-02-11 22:42:57 +0100

    [worker] upgrade mathjax Fixes #42

--- src/worker.js
+++ src/worker.js
@@ -37,7 +37,7 @@ else {
   };
   const mj = document.createElement('script');
   mj.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_CHTML-full';
+    'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-AMS_CHTML-full';
   document.head.appendChild(mj);
 
 }
