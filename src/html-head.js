193145ace8aecfe1b8d8ad3213d25688773fe4ab
Peter Krautzberger <p.krautzberger@gmail.com>
2018-02-23 22:50:27 +0100

    [html-head] accept document object enables nodejs processing Fixes #49

--- src/html-head.js
+++ src/html-head.js
@@ -1,6 +1,5 @@
-const metaTagGenerator = function(){
-    // expects at least one string of type `attribute="value"`
-    const argArr = [...arguments];
+const metaTagGenerator = function(document, argArr){
+    // expects array of strings of type `attribute="value"`
     const checkForElement = document.querySelector('head [' + argArr.join('][') + ']');
     if (!checkForElement){
         const metaElement = document.createElement('meta');
@@ -16,10 +15,10 @@ const metaTagGenerator = function(){
 module.exports = function(document) {
     // some good practices (especially for fragments)
     // add <meta charset="UTF-8">
-    metaTagGenerator('charset="UTF-8"');
+    metaTagGenerator(document, ['charset="UTF-8"']);
     // <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    metaTagGenerator('name="viewport"', 'content="width=device-width, initial-scale=1.0"')
+    metaTagGenerator(document, ['name="viewport"', 'content="width=device-width, initial-scale=1.0"'])
     // <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    metaTagGenerator('http-equiv="X-UA-Compatible"', 'content="ie=edge"');
+    metaTagGenerator( document, ['http-equiv="X-UA-Compatible"', 'content="ie=edge"']);
 
 };
