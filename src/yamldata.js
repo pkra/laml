b7387872ad3907f408353240a65b6f288c94308c
Peter Krautzberger <p.krautzberger@gmail.com>
2018-02-23 22:28:50 +0100

    [yamldata.js] add error handling and clean up Fixes #46

--- src/yamldata.js
+++ src/yamldata.js
@@ -2,12 +2,16 @@ const yaml = require('js-yaml');
 
 module.exports = function(document) {
   // convert yaml to json metadata
-  // TODO think about schema.org markup
-  const yamldata = yaml.safeLoad(document.querySelector('script[type="application/yaml"]').text);
+  // TODO read first text node if it looks like YAML
+  const yamlNode = document.querySelector('script[type="application/yaml"]');
+  if (!yamlNode) return ;
+  const yamldata = yaml.safeLoad(yamlNode.text);
   if (!yamldata) return ;
   const metadata = document.createElement('script');
   metadata.setAttribute('type', 'application/json');
   metadata.setAttribute('data-laml', 'metadata');
   metadata.text = JSON.stringify(yamldata);
   document.head.appendChild(metadata);
+  // clean up
+  yamlNode.parentNode.removeChild(yamlNode);
 };
